b14d71e2632a82b64dd9679c81de4492ea7801de
Azynux <aeziotech@gmail.com>
2022-08-29 22:58:04 +0200

    Command interaction detection patch

--- src/events/interactions/interactionCreate.js
+++ src/events/interactions/interactionCreate.js
@@ -7,16 +7,13 @@ const { Client, Interaction, InteractionType } = require('discord.js');
 module.exports = async (client, interaction) => {
     console.log(interaction.type);
     if(interaction.type === InteractionType.ApplicationCommand) {
-        const subcommandname = interaction.options.getSubcommand();
-        
-        if(subcommandname) {
+        try {
+            const subcommandname = interaction.options.getSubcommand();
             const execute = client.commands.find(cmd => cmd.name == subcommandname && cmd.groupname == interaction.commandName)?.execute;
             if(typeof execute == "function") execute(client, interaction);
-            return;
-        } else {
+        } catch {
             const execute = client.commands.find(cmd => cmd.name == interaction.commandName && !cmd.groupname)?.execute;
             if(typeof execute == "function") execute(client, interaction);
-            return;
         };
     };
 };
