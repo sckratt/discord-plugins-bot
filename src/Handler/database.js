6128da4ac5f0e8884738e2f9eac32de85783d58e
Azynux <aeziotech@gmail.com>
2022-08-30 18:21:28 +0200

    database structure changed for guild Ids

--- src/Handler/database.js
+++ src/Handler/database.js
@@ -1,13 +1,14 @@
 const { Client } = require('discord.js');
 const { QuickDB } = require('quick.db');
 const path = require('path');
+const config = require("./" + path.relative(__dirname, path.join(process.cwd(), "config.json")));
 
 /**
  * @param {string} dbname
  */
 function db(dbname) {
     if(!dbname) throw new Error("Missing parameters: The database name must be provided");
-    const db = new QuickDB({ filePath: path.resolve(process.cwd(), 'data/' + dbname) });
+    const db = new QuickDB({ filePath: path.resolve(process.cwd(), 'data/' + config.guildId + "/" + dbname) });
 
     return db;
 };
