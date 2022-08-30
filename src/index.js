7058f86c7573c822c6cdec876854afd072b4a27a
Azynux <aeziotech@gmail.com>
2022-08-30 18:22:49 +0200

    config checking and storing in client cache

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
+const config = require("./" + path.relative(__dirname, path.join(process.cwd(), "config.json")));
+
+if(!config.guildId) throw new Error("Missing config value: Guild ID is not provided !");
 
 const { Client, IntentsBitField, Partials } = require('discord.js');
 
@@ -8,6 +11,7 @@ const client = new Client({
     intents: Object.values(IntentsBitField.Flags),
     partials: Object.values(Partials)
 });
+client.config = config;
 client.db = require('./Handler/database.js').db;
 
 
