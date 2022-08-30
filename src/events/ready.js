61197d0b886976468ae253f1ae6968451ecfb8b0
Azynux <aeziotech@gmail.com>
2022-08-30 18:21:53 +0200

    Check for guild availability

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,13 @@
 const { Client } = require('discord.js');
+const path = require('path');
 
 /**
  * @param {Client} client 
  */
 module.exports = async (client) => {
+    const guild = client.guilds.cache.get(client.config.guildId);
+    if(!guild || !guild.available) console.log("[!] Guild with id " + client.config.guildId + " is not available.");
+
     console.log(`Logged in as ${client.user.tag}`);
     require('../Handler/commands').loadPluginsCommands(client);
     require('../Handler/commands').loadCommands(client);
