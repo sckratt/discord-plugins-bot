8e506ff06a17bfd690b879726b09a750309e25c2
Azynux <aeziotech@gmail.com>
2022-08-31 11:22:50 +0200

    modules handler patch

--- src/Handler/modules.js
+++ src/Handler/modules.js
@@ -7,10 +7,10 @@ const pkg = require('../../package.json');
 fs.readdirSync(path.resolve(process.cwd(), "plugins"))
 .filter( f => fs.statSync(path.resolve(process.cwd(), "plugins/" + f)).isDirectory() )
 .forEach(folder => {
-    if(!( fs.existsSync(path.resolve(process.cwd(), "plugins/" + f + "/package.json")) && fs.statSync(path.resolve(process.cwd(), "plugins/" + f + "/package.json")).isFile() )) return;
-    const package = require(path.relative(__dirname, path.resolve(process.cwd(), "plugins/" + f + "/package.json")));
+    if(!( fs.existsSync(path.resolve(process.cwd(), "plugins/" + folder + "/package.json")) && fs.statSync(path.resolve(process.cwd(), "plugins/" + f + "/package.json")).isFile() )) return;
+    const package = require(path.relative(__dirname, path.resolve(process.cwd(), "plugins/" + folder + "/package.json")));
     if(!package.dependencies?.length) return;
-    
+
     Object.keys(package.dependencies)
     .filter(d => !Object.keys(pkg.dependencies).includes(d))
     .forEach(d => {
