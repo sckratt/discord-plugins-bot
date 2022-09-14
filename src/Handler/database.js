dc8df9215e5aac3c0b1ae0e3a8a432cfc953a422
Azynux <aeziotech@gmail.com>
2022-09-14 15:23:36 +0200

    database creation error patched

--- src/Handler/database.js
+++ src/Handler/database.js
@@ -10,7 +10,7 @@ const config = require(path.relative(__dirname, path.join(process.cwd(), "config
 function db(dbname) {
     if(!dbname) throw new Error("Missing parameters: The database name must be provided");
     if(!fs.existsSync(path.resolve(process.cwd(), "data/" + config.guildId)))
-        fs.mkdirSync(path.resolve(process.cwd(), "data/" + config.guildId))
+        fs.mkdirSync(path.resolve(process.cwd(), "data/" + config.guildId), { recursive: true })
     const db = new QuickDB({ filePath: path.resolve(process.cwd(), 'data/' + config.guildId + "/" + dbname + ".sqlite") });
 
     return db;
